1c740231d0112662da4e0683f3993d2bf076b391
ajcudd <ajcuddeback@gmail.com>
2021-01-25 15:05:52 -0500

    added get route per an if

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,6 +29,31 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     // find one category by its `id` value
     // be sure to include its associated Products
+    Category.findOne(
+        {
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: Product,
+                    attributes: ['id', 'product_name', 'price', 'stock']
+                }
+            ]
+        }
+    )
+        .then(dbCategoryData => {
+            if (!dbCategoryData) {
+                res.status(404).json({ message: 'No user at this id' });
+                return;
+            }
+
+            res.json(dbCategoryData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
 });
 
 router.post('/', (req, res) => {
